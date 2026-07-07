fcb682973bfe68cc72cc5d6765347b1c9dc74a96
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-08 01:21:14 +0300

    variables & datatypes

--- variables.js
+++ variables.js
@@ -1,3 +1,31 @@
 //variables = a container that stores a value.
 // 1. declaration let x;
 // 2. assignement x = 100;
+
+// working with integers datatypes
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log("you are ${age} years old");
+console.log("the price of the book is $${price} ");
+console.log("your gpa is: ${gpa}");
+
+//working with strings
+let firstName = "peter";
+let favoriteFood = "PIZZA";
+let email = "broaaa@gmail.comm";
+document.getElementById;
+("p1");
+textcontent = "your name is ${firstName}";
+
+// show the kind of datatyoe you are working with use
+console.log(typeof firstName);
+console.log("you love eating ${favoriteFood}");
+console.log("your email is ${email}");
+
+//working with booleans, they are either tru or false
+let online = true;
+let isKids = false;
+console.log("Bro is online: ${online}");
+consloge.log("school is for kids: ${isKids}");
