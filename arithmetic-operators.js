cd10251a18fcc9dc208b5375eb2221ee768fe2b5
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-08 01:37:28 +0300

    arithmetic operators

---
+++ arithmetic-operators.js
@@ -0,0 +1,31 @@
+// arithmetic operators = operands (values, variables, etc.)
+//                        operators (+ - * /)
+//                        ex. 11=x+5;
+let students = 30;
+
+students = studens + 2; // addition operator
+students = students - 3; // subtraction operator
+students = students / 2; // division operator;
+students = students * 7; // multiplication;
+students = students ** 2; // exponential operator
+students = students % 2; // modulus/ remainder operator
+
+console.log(students);
+
+// augmented assignment operators
+students += 2; //adds
+students -= 3; // subtracts
+students *= 7; //multiplies
+students /= 2; //divides
+students **= 2; //exponential
+students %= 2; //remainder returns
+
+// incremental and decremental operators = increasing a value by one , decreasing value by one
+students++;
+students--;
+
+//operator precedence
+// 1. parenthesis ()
+// 2. exponents
+// 3. multiplication & division & modulo
+// 4. addition & subtraction
