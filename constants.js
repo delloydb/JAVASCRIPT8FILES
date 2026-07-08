779eb65943628f8054b8b613f1ffb35fdd8307db
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-09 00:29:01 +0300

    constants

---
+++ constants.js
@@ -0,0 +1,11 @@
+// const = are variables that cant be changed
+const PI = 3.148;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter radius of circle");
+radius = Number(radius);
+
+circumference = 2 * PI * radius;
+
+console.log(circumference);
