47478958757c5222a59624f5919a30755e44dcff
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-13 00:20:04 +0300

    math object

---
+++ mathobject.js
@@ -0,0 +1,22 @@
+// math = to buit-in object that provides a collection of properties and methods
+
+let a = 5.56;
+let b = 4;
+let c = 3;
+let d;
+
+d = math.round(a); // round off function
+d = Math.floor(a); //round off down function
+d = Math.ceil(a); //round off up function
+d = Math.trunc(a); // truncate function
+d = Math.max(a, b, c); // returns largest value
+d = Math.min(a, b, c); //returns smallest value
+d = Math.pow(b, c); //returns the value of b to the power of c
+d = Math.sin(c); //returns the sine of c
+d = Math.tan(b); //returns the tnagent of b
+d = Math.cos(b); //returns the cosine of b
+d = Math.abs(a); //eturns the absolute value of a
+d = Math.sqrt(b); // retunrs the square root of b
+d = Math.sign(a);
+
+console.log(d);
