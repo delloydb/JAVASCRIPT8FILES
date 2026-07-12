ad8224ad7ddc6df1293fdcbc4636da2d4a18d66c
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-12 22:32:43 +0300

    constants

--- constants.js
+++ constants.js
@@ -9,3 +9,10 @@ radius = Number(radius);
 circumference = 2 * PI * radius;
 
 console.log(circumference);
+
+document.getElementById("sub").onclick = function () {
+  radius = document.getElemntById("rad").value;
+  radius = Number(radius);
+  circumference = 2 * PI * radius;
+  document.getElementById("h1").textContent = cicurmference + "cm";
+};
