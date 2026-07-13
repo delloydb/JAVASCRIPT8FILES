0cdb32b5e2c7a6aad49680e3914ca6fa83579868
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-13 13:18:48 +0300

    random number generation

---
+++ randomnumber.js
@@ -0,0 +1,10 @@
+// generating random numbers
+
+const max = 100; // setting limits to ne reached by the generator function
+const min = 20;
+
+let randomNumber = Math.floor(Math.random() * max) + min;
+
+console.log(randomNumber);
+
+// useful for games like dice rolling
