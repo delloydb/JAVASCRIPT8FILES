35f683f4fc7046e01df7281ef264f5a351dc9459
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-14 13:40:24 +0300

    strict equality operator

---
+++ strict-equality.js
@@ -0,0 +1,13 @@
+// " = " this is the assignment operator
+// " == " this is the comparision operator (checks whether some values are equal)
+// " != " this is the ineguality operator
+// " !== " this the strict inequality operator
+// " === " this is the strict equality operator
+
+const PI = 3.14;
+
+if (PI === "3.14") {
+  console.log("thats not PI");
+} else {
+  console.log("thats PI");
+}
