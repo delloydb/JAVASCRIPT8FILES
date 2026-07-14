75bee76b115b51d9745a64503bdfe9e1a6b6cda0
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-14 13:12:53 +0300

    method chaining

---
+++ chaining.js
@@ -0,0 +1,15 @@
+// method chaining = calling one method after another in a conitnous line of code
+
+// no method chaining.......
+let username = window.prompt("Enter your username: ");
+
+username = username.trim();
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
+
+let extraChars = username.slice(1);
+extraChars = extraChars.toLowerCase();
+username = letter + extraChars;
+
+console.log(username);
+// method chaining........
