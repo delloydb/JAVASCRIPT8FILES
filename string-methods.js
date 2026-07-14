504746feeadafa20dbd07df54fe212e66d04fa55
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-14 12:38:57 +0300

    string methods

---
+++ string-methods.js
@@ -0,0 +1,36 @@
+// string methods are functions that allow you manipulate and work with text (strings)
+
+let username = "BroCode";
+
+//getting character of a string @ using the charAt() method
+
+console.log(userName.charAt(0)); //returns B
+
+//getting the index of the occurence of a character in a string using indexOf() method
+
+console.log(userName.indexOf("o")); //returns 2
+
+console.log(userName.lastindexOf("o")); //returns 4
+
+//getting the length of a string
+
+console.log(userName.length);
+
+//triming a string
+
+console.log(userName.trim());
+
+//changing the letter case of a string
+
+console.log(userName.toUpperCase());
+
+console.log(userName.toLowerCase());
+
+//causing a repeat use repeat()
+
+console.log(userName.repeat());
+
+//check whether a string starts with something.
+
+let result = userName.startsWith(" ");
+console.log(result);
