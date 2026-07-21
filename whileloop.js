d8c5720985df468e3fef9bec184f2106c8ba9daa
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-21 16:20:26 +0300

    while and do while

--- whileloop.js
+++ whileloop.js
@@ -1,6 +1,6 @@
 // while loop is a conditional statement that executes a program infinitely while some condition is true
 
-// example
+// example while loop
 
 let username;
 
@@ -10,8 +10,39 @@ while (username === " ") {
 
 console.log("Name is missing");
 
+//do while loop counter for while loop that allows to execute one condition as the rest may not be true
 do {
   username = window.prompt("enter name");
 } while (username === " ");
 
 console.log("Name is missing");
+
+//while loop for login
+let loggedIn = false;
+let username;
+let password;
+
+while (!loggedIn) {
+  username = window.prompt("enter your username");
+  passord = window.prompt("enter your password");
+
+  if (username === "myUsername" && passord === "myPassword") {
+    loggedIn = true;
+    console.log("you are logged in");
+  } else {
+    console.log("invalid credentials");
+  }
+}
+
+//do while for login
+do {
+  username = window.prompt("enter your username");
+  passord = window.prompt("enter your password");
+
+  if (username === "myUsername" && passord === "myPassword") {
+    loggedIn = true;
+    console.log("you are logged in");
+  } else {
+    console.log("invalid credentials");
+  }
+} while (!loggedIn);
