6f8f42c5652973a160bd1a37feabffaef883e1d4
Lloyd Brown <lloydbrownjnr@gmail.com>
2026-07-23 13:14:48 +0300

    functions

---
+++ functions.js
@@ -0,0 +1,9 @@
+// functions - a section of resusable code. Declare code once and use it whenever you want, by calling a function to execute that code
+
+//function declaration
+function happyBirthday() {
+  console.log("happy birthday to you!");
+}
+
+//function call
+happyBirthday();
